0a3ea4efa6d699f57708c6a8f5b11d0d4b608946
Brittney Alfieri <vbrit95@hotmail.com>
2023-04-08 21:47:19 -0400

    Project initial updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,24 @@
 import react from "react";
-import headerImage from "../images/header-image.jpg";
 
 const Header = () => {
   return (
     <div className="header">
-      <div className="header__header">
-        <p>The Green Parakeet Grill & Pizza</p>
-      </div>
-      <div className="header__info">
-        <h1>Fresh, Local, Artisan cuisine</h1>
-        <h2>
-          The Green Parakeet Grill & Pizza prepares good but simple food. From
-          pizza to local caught fish, we have something for everyone.
-        </h2>
-      </div>
-      <div className="header__footer">
-        <button className="header__button" type="button">
-          Order Now
-        </button>
+      <div className="header__wrapper">
+        <div className="header__header">
+          <p className="header__title">The Green Parakeet Grill & Pizza</p>
+        </div>
+        <div className="header__info">
+          <h1 className="header__info-title">Fresh, Local, Artisan cuisine</h1>
+          <h2 className="header__info-paragraph">
+            The Green Parakeet Grill & Pizza prepares good but simple food. From
+            pizza to local caught fish, we have something for everyone.
+          </h2>
+        </div>
+        <div className="header__footer">
+          <button className="header__button" type="button">
+            Order Now
+          </button>
+        </div>
       </div>
     </div>
   );
