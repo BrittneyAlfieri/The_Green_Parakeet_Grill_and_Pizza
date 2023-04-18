4c99fa2ea86ef1c972283385deefcfb5e6a57e4d
Brittney Alfieri <vbrit95@hotmail.com>
2023-04-17 21:03:30 -0400

    initial commits

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,7 +1,21 @@
 const SocialMedia = () => {
+  const iframe = `<iframe src="https://snapwidget.com/embed/1031005" class="snapwidget-widget" allowtransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden;  width:765px; height:510px"></iframe>`;
+
+  function Iframe(props) {
+    return (
+      <div
+        dangerouslySetInnerHTML={{ __html: props.iframe ? props.iframe : "" }}
+      />
+    );
+  }
+
   return (
     <section className="socialmedia">
-      <div className="socialMedia__wrapper"></div>
+      <div className="socialMedia__wrapper">
+        <div className="socialmedia__iframe">
+          <Iframe iframe={iframe} />
+        </div>
+      </div>
     </section>
   );
 };
