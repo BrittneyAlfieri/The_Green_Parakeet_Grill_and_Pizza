5f55189a2d82eb49ce7086a2d9dfdf5ad0c016c1
Brittney Alfieri <vbrit95@hotmail.com>
2023-05-01 18:35:30 -0400

    initial project commits

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,13 +8,13 @@ const Services = () => {
         <div className="services__page">
           <img src={servicesImage} className="services__image" />
           <div className="services__info">
-            <div className="services__title">Our services</div>
-            <p className="services__paragraph">01. Dine on site</p>
+            <div className="services__title">Our Services</div>
+            <p className="services__paragraph">01. Dine on site.</p>
             <p className="services__paragraph">
-              02. Order takeout via phone or online ordering
+              02. Order takeout via phone or online ordering.
             </p>
             <p className="services__paragraph">
-              03. Enjoy our selection of eclectic wines and craft beers
+              03. Enjoy our selection of eclectic wines and craft beers.
             </p>
           </div>
         </div>
