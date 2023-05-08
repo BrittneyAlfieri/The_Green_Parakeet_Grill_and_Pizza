fc8afe9257813a9024e500cf180c6aac7a996ae9
Brittney Alfieri <vbrit95@hotmail.com>
2023-05-08 12:09:41 -0400

    Project 12 initial commits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,5 @@
+import mapImage from "../images/map.png";
+
 const Footer = () => {
   return (
     <div className="footer">
@@ -5,10 +7,12 @@ const Footer = () => {
         <div className="footer__info">
           <h2 className="footer__header">Contact Us</h2>
           <p className="footer__text">
-            The Green Parakeet Grill & Pizza 814 NE 6th Avenue Delray Beach,
-            Florida, 33483 561-528-7063
+            814 NE 6th Avenue Delray Beach, Florida, 33483.
           </p>
+          <p className="footer__text">(561)-528-7063</p>
+          <p className="footer__text">Wednesday - Saturday: 5pm to 9pm</p>
         </div>
+        <img className="footer__map" src={mapImage}></img>
       </div>
     </div>
   );
